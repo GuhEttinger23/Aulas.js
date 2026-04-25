3f07da366bc472dd306e0c220e050a3d918fd111
Gustavo Ettinger Bernardes <gustafelipao2308@gmail.com>
2026-04-25 18:44:58 -0300

    Add files via upload

---
+++ aula 11/aula11.js
@@ -0,0 +1,54 @@
+// let btn1 = document.getElementById('btn1');
+
+
+// btn1.addEventListener('click', function() {
+//     alert('Meu Popup aberto com JS')
+//     } 
+// )
+
+
+// // atividade 1
+
+// const botao = document.getElementById("meuBotao");
+
+// botao.addEventListener("click", function() {
+//     alert("Botão clicado!");
+// });
+
+
+// btn1.addEventListener('click', function() {
+//     let novoTexto = document.createElement('p')
+
+//     novoTexto.innerText = 'Esse paragrafo foi criado com JS'
+
+//     document.body.appendChild(novoTexto)
+//     } 
+// )
+
+
+// atividade 2
+
+
+// const formulario = document.getElementById("meuFormulario");
+// const container = document.getElementById("containerCards");
+
+// formulario.addEventListener("submit", function(event) {
+//     event.preventDefault();
+
+//     const titulo = document.getElementById("titulo").value;
+//     const descricao = document.getElementById("descricao").value;
+
+//     const card = document.createElement("div");
+//     const tituloCard = document.createElement("h3");
+//     const descricaoCard = document.createElement("p");
+
+//     tituloCard.textContent = titulo;
+//     descricaoCard.textContent = descricao;
+
+//     card.appendChild(tituloCard);
+//     card.appendChild(descricaoCard);
+
+//     container.appendChild(card);
+
+//     formulario.reset();
+// });
